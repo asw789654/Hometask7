18d51d45bc9324356e7a2d7b5b9b7f200d251baf
iliuha135@gmail.com <iliuha135@gmail.com>
2023-11-01 06:32:33 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task1;
+//проверки
+Sphere sphere = new Sphere(2, 2, 2, 2);
+sphere.SetCenter(3, 3, 3);
+Console.WriteLine(sphere.Radius);
+Console.WriteLine(sphere.GetCenter());
+Console.WriteLine(sphere.GetSquare());
+Console.WriteLine(sphere.GetVolume());
+sphere.Radius = 4;
+Console.WriteLine(sphere.Radius);
+Console.WriteLine(sphere.IsPointInside(2, 9, 2));
+
---
+++ Task1/Sphere.cs
@@ -0,0 +1,50 @@
+﻿namespace Task1
+{
+    internal class Sphere
+    {
+        private double x, y, z, radius;
+        public Sphere(double x = 0, double y = 0, double z = 0, double radius = 1)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.radius = radius;
+        }
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+        public double GetVolume()
+        {
+            return 4.0 / 3.0 * Math.PI * Math.Pow(radius, 3);
+        }
+        public double GetSquare()
+        {
+            return 4 * Math.PI * Math.Pow(radius, 2);
+        }
+        public string GetCenter()
+        {
+            return $"{x} {y} {z}";
+        }
+        public void SetCenter(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public bool IsPointInside(double x, double y, double z)
+        {
+            bool result = false;
+            double newRadius = Math.Sqrt(
+                  ((x - this.x) * (x - this.x))
+                + ((y - this.y) * (y - this.y))
+                + ((z - this.z) * (z - this.z)));
+            if (newRadius <= radius)
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
